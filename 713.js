e7e1b2ccfea92ef332a48fc579c61fbe470d40db
bunju20 <ghkd2271@naver.com>
2022-03-27 14:16:11 +0900

    Algo : 0326

--- 713.js
+++ 713.js
@@ -3,6 +3,27 @@ solution:
 time:
 space:
 
+ 연속적인 그룹개수 -> sliding window 계산가능.
+ [10,5,2,6]
+     ^
+       ^
+
+  => [10], 1개
+  => 50 [5], [10,5] => 2개
+  => 10 [2] [5,2]... 
+  sliding window의 너비만큼을 계속 더해주면 그게 그룹개수.
+
+  solution
+  - while(r<길이)
+    - prod*=r의 값
+    - count++
+    - while(곱 < k) 곱에 왼쪽 나눠주고, l++, count--;
+    - res += count
+  - r++;
+
+  time:O(N)
+  space: O(1)
+
 */
 
 var numSubarrayProductLessThanK = function (nums, k) {
