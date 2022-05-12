bf3b7112c9dab26b8e1c88f9c055648d5839b23a
bunju <ghkd2271@naver.com>
2022-05-12 22:22:20 +0900

    algo

--- 1101.js
+++ 1101.js
@@ -9,36 +9,50 @@ Union (합침 이라는 뜻.)
 부모가 모두 1이르모 세가지의 노드는 모두 같은 그래프에 있다고 판단할수 있다.
 
 
+CODE //부모노드를 찾는 함수
+해당 부모의 값과 같으면 x 자체를 반환하고 - end clause
+둘이 다르면 실제를 찾기위해서 재귀를 돌려준다.
+
+CODE //부모노드를 합치는 함수
+각의 부모값을 구한뒤에
+더 작은 값 쪽으로 부모를 합쳐준다.
+
+CODE //같은 부모를 가지는지 확인함.
+둘이 같으면 1반환
+아니면 0반환..
+
+
 
 */
 var earliestAcq = function(logs, N) {
-    logs.sort((a,b) => a[0] - b[0]);
+    logs.sort((a,b) => a[0] - b[0]); //오름차순으로 정렬.
 
     const ids = [], size = [];
     for(let i = 0; i < N; i++) { ids.push(i); size.push(1); }
-    
-    const union = (root1, root2) => {
-        if (size[root2] > size[root1]) union(root2, root1);
-        ids[root2] = root1;
-        size[root1] += size[root2];
-    }
-    
-    const find = (id) => {
-        while (ids[id] != id) {
-            ids[id] = ids[ids[id]];
-            id = ids[id];
-        }
-        return id;
-    }
-    
+
     for(let log of logs) {
-        const root1 = find(log[1]);
-        const root2 = find(log[2]);
+        const root1 = find(log[1],ids); //최대의 부모 노드 값 받아옴.
+        const root2 = find(log[2],ids);
+
         if (root1 != root2) {
-            union(root1, root2);
+            union(root1, root2,size,ids); //합쳐줌
             if (--N == 1)
                 return log[0];
         }
     }
     return -1;
 };
+
+const union = (root1, root2,size,ids) => {
+    if (size[root2] > size[root1]) union(root2, root1 , size, ids);
+    ids[root2] = root1;
+    size[root1] += size[root2];
+}
+
+const find = (id,ids) => {
+    while (ids[id] != id) {
+        ids[id] = ids[ids[id]];
+        id = ids[id];
+    }
+    return id;
+}
--- 1245.js
+++ 1245.js
@@ -7,29 +7,45 @@ E:
 ds:
 algo:
 solution:
-root : 1
 
-0 - 1(1) - 2 (2) - 3(3)
-          - 4(2) - 5(3)
-0,3,5
+0:1
+1:2,4
+2:3
+4:5
 
-time:
-space:
+한 노드당 이어져있을수 있는 노드 갯수 구하고 최대 노드갯수 - 1반환하면 됨.
+
 */
 
-var treeDiameter = function (edges) {
-    let leaf = [];
-    let map = {};
-    for(let[x,y] of edges){
-        if(x in map)map[x]++;
-        else map[x] = 1;
-        if (y in map) map[y]++;
-        else map[y] = 1;
-    }
+var treeDiameter = function(edges) {
+    const store = {};
+    edges.forEach(([a, b]) => {
+      if (store[a]) store[a].push(b);
+      else store[a] = [b];
+      if (store[b]) store[b].push(a);
+      else store[b] = [a];
+    });
+    let res = 0;
+    const seen = new Set();
+  
+  
+    dfs(0,seen,store,res);
+    return res - 1;
+  };
 
-    for(let [key, val] of Object.entries(map)){
-        let pushNum = parseInt(key);
-        if(val === 2)leaf.push(pushNum);
+  function dfs(node,seen,store,res){
+    seen.add(node);
+
+    let [first, second] = [0, 0];
+    for (let child of store[node]) {
+      if (seen.has(child)) continue;
+      const pathLength = dfs(child,seen,store,res);
+      if (pathLength > first) {
+        second = first;
+        first = pathLength;
+      } else if (pathLength <= first && pathLength > second)
+        second = pathLength;
     }
-    
-};
+    res = Math.max(res, first + second + 1);
+    return 1 + first;
+  };
