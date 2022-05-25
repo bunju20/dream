afea9067e127c0f15ceadbd1e1015fb30765c597
bunju <ghkd2271@naver.com>
2022-05-25 16:10:01 +0900

    JS : home contact btn

--- main.js
+++ main.js
@@ -24,8 +24,17 @@ navbarMenu.addEventListener('click',(event)=>{
     if(link == null){
         return;
     }
-    console.log(event.target.dataset.link);
-    const scrollTo = document.querySelector(link);
-    scrollTo.scrollIntoView({behavior: "smooth"});
-
+    scrollIntoView(link);
 })
+
+// contact me 누르면 contact로 이동시켜줌.
+
+const contactMe = document.querySelector('.home__contact');
+ contactMe.addEventListener('click',(event)=>{
+    scrollIntoView('#contact')
+ })
+
+ function scrollIntoView(selector){
+    const scrollTo = document.querySelector(selector);
+    scrollTo.scrollIntoView({behavior: "smooth"});
+ }
