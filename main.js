4dc0269a1aac155d9a22ba7d74fa628ad0c4da5b
bunju <ghkd2271@naver.com>
2022-05-25 15:49:48 +0900

    JS : navbar menu click

--- main.js
+++ main.js
@@ -2,7 +2,9 @@
 
 // navbar을 스크롤이 내릴때마다 투명에서 불투명으로 바꿉니다.
 const navbar = document.querySelector('#navbar');
+
 const navbarHeight = navbar.getBoundingClientRect().height;
+// 스크롤이 될때마다 이 안에있는 애가 실행이 되도록.
 document.addEventListener('scroll',()=>{
     console.log(window.scrollY);
     console.log(`navbarHeight: ${navbarHeight}`);
@@ -11,4 +13,19 @@ document.addEventListener('scroll',()=>{
     }else{
         navbar.classList.remove('navbar--dark');
     }
+})
+
+// navbar누르면 스크롤링해주는 거
+
+const navbarMenu = document.querySelector('.navbar__menu');
+navbarMenu.addEventListener('click',(event)=>{
+    const target = event.target;
+    const link = target.dataset.link;
+    if(link == null){
+        return;
+    }
+    console.log(event.target.dataset.link);
+    const scrollTo = document.querySelector(link);
+    scrollTo.scrollIntoView({behavior: "smooth"});
+
 })
